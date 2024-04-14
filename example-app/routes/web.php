76e33c1fa52c0cec403785ab18594cffbc46d3e7
Minhhiep222 <minhhiep325@gmail.com>
2024-04-14 07:59:07 +0700

    add

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Vite;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('header', function () {
+    return view('header');
 });
