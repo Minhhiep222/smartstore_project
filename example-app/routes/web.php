6df580ceb147c40ae2f25ea04f8f4c879a34a90a
DucToan0208 <nhoctoan6666@gmail.com>
2024-04-14 13:59:40 +0700

    Tao them file cart ne

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('cart', function () {
+    return view('cart');
+});
+Route::get('item', function () {
+    return view('item');
+});
